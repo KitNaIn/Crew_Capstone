86aabe2f18fb2969570b81c2960482dc1c1ae9eb
KitNaIn <k.na-in@gmx.de>
2023-06-19 22:37:18 +0200

    adjusted pom dependencys

--- backend/src/main/java/de/neuefische/backend/repo/JobRepo.java
+++ backend/src/main/java/de/neuefische/backend/repo/JobRepo.java
@@ -2,10 +2,10 @@ package de.neuefische.backend.repo;
 
 import de.neuefische.backend.model.Job;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 
-@Repository
+
+
 public interface JobRepo extends MongoRepository <Job, String> {
 
 }
