a448781d1d8cb2645b63393c4042221e86a2bf5d
KitNaIn <k.na-in@gmx.de>
2023-06-19 23:54:00 +0200

    added uuidservice test

---
+++ backend/src/test/java/de/neuefische/backend/controller/JobControllerTest.java
@@ -0,0 +1,27 @@
+package de.neuefische.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+
+class JobControllerTest {
+
+    @Test
+    void getAllJobs() {
+    }
+
+    @Test
+    void getAllJobsSortedByDateTime() {
+    }
+
+    @Test
+    void saveJob() {
+    }
+
+    @Test
+    void updateJobStatus() {
+    }
+
+    @Test
+    void deleteJob() {
+    }
+}
