15d669fbb305ec5a897bc71e2fa55c5d42798bf0
KitNaIn <k.na-in@gmx.de>
2023-06-20 16:31:21 +0200

    added findbyid update sort and delete endpoint

--- backend/src/main/java/de/neuefische/backend/service/JobService.java
+++ backend/src/main/java/de/neuefische/backend/service/JobService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +18,8 @@ public class JobService {
     public List<Job> findAll() {
         return jobRepo.findAll();
     }
-    public List<Job> save(Job job) {
 
+    public List<Job> save(Job job) {
         Job newJob = Job.builder()
                 .uuid(generateUUIDService.generateUUID())
                 .jobFormat(job.getJobFormat())
@@ -35,6 +36,7 @@ public class JobService {
         jobRepo.save(newJob);
         return findAll();
     }
+
     public Job convertToJobEntity(JobDto jobDto) {
         return Job.builder()
                 .jobFormat(jobDto.getJobFormat())
@@ -46,6 +48,43 @@ public class JobService {
                 .endTime(jobDto.getEndTime())
                 .jobComment(jobDto.getJobComment())
                 .build();
+    }
+
+    public List<Job> getAllJobsSortedByDateTime() {
+        List<Job> allJobs = jobRepo.findAll();
+        allJobs.sort((job1, job2) -> {
+            int dateComparison = job1.getJobDate().compareTo(job2.getJobDate());
+            if (dateComparison == 0) {
+                return job1.getStartTime().compareTo(job2.getStartTime());
+            }
+            return dateComparison;
+        });
+        return allJobs;
+    }
+
+    public Job findJobById(String id) {
+        return jobRepo.findById(id).orElseThrow(() -> new NoSuchElementException("Event with id " + id + " does not exist"));
+    }
+    public Job updateJob(String id, Job job) {
+        Job jobToUpdate = findJobById(id);
+        if (jobToUpdate != null) {
+            jobToUpdate.setJobFormat(job.getJobFormat());
+            jobToUpdate.setIssuer(job.getIssuer());
+            jobToUpdate.setContactPerson(job.getContactPerson());
+            jobToUpdate.setJobAddress(job.getJobAddress());
+            jobToUpdate.setJobDate(job.getJobDate());
+            jobToUpdate.setStartTime(job.getStartTime());
+            jobToUpdate.setEndTime(job.getEndTime());
+            jobToUpdate.setJobComment(job.getJobComment());
+            return jobRepo.save(jobToUpdate);
+        } else {
+            throw new IllegalArgumentException("Job not found");
+        }
+    }
+
+
+    public Job deleteJobById (String id) {
+        return jobRepo.findById(id).orElseThrow(() -> new NoSuchElementException("Event with id " + id + " does not exist"));
     }
 
 }
--- backend/src/test/java/de/neuefische/backend/controller/JobControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/JobControllerTest.java
@@ -1,12 +1,24 @@
 package de.neuefische.backend.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import de.neuefische.backend.model.Job;
+import de.neuefische.backend.service.GenerateUUIDService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.net.http.WebSocketHandshakeException;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -16,13 +28,154 @@ class JobControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
     @Test
     void getAllJobs_ShouldReturnEmptyList() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
     }
+
+
+    @Test
+    void saveJob_ShouldReturnJobWithId_andStatusCode200() throws Exception {
+        //GIVEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/jobs")
+                        .contentType("application/json")
+                        .content("""
+                                    {
+                                        "jobFormat": "Format",
+                                        "issuer": "Issuer",
+                                        "contactPerson": "Contact Person",
+                                        "jobAddress": "Job Address",
+                                        "jobDate": "2023-06-14",
+                                        "startTime": "10:00:00",
+                                        "endTime": "12:00:00",
+                                        "status": null,
+                                        "jobComment": "Job Comment"
+                                    }
+                                """))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].uuid").isNotEmpty())
+                .andExpect(status().isCreated())
+                .andReturn();
+        //THEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+            [
+                {
+                    "jobFormat": "Format",
+                    "issuer": "Issuer",
+                    "contactPerson": "Contact Person",
+                    "jobAddress": "Job Address",
+                    "jobDate": "2023-06-14",
+                    "startTime": "10:00:00",
+                    "endTime": "12:00:00",
+                    "status": null,
+                    "jobComment": "Job Comment"
+                }
+            ]
+        """))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].uuid").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].uuid").isNotEmpty());
+    }
+
+
+    @Test
+    void getAllJobsSortedByDateTime_ShouldReturnSortedJobs_andStatusCode200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/jobs")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                                    {
+                                        "jobFormat": "Format",
+                                        "issuer": "Issuer",
+                                        "contactPerson": "Contact Person",
+                                        "jobAddress": "Job Address",
+                                        "jobDate": "2023-06-14",
+                                        "startTime": "10:00:00",
+                                        "endTime": "12:00:00",
+                                        "status": null,
+                                        "jobComment": "Job Comment"
+                                    }
+                                """));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/jobs")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                                    {
+                                        "jobFormat": "Format",
+                                        "issuer": "Issuer",
+                                        "contactPerson": "Contact Person",
+                                        "jobAddress": "Job Address",
+                                        "jobDate": "2022-05-13",
+                                        "startTime": "10:00:00",
+                                        "endTime": "12:00:00",
+                                        "status": null,
+                                        "jobComment": "Job Comment"
+                                    }
+                                """));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs/sorted"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].jobDate").value("2022-05-13"))
+                .andExpect(jsonPath("$[1].jobDate").value("2023-06-14"));
+    }
+
+    @Test
+    void updateJob_ShouldReturn_UpdatedJob() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/jobs")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "jobFormat": "Format",
+                                    "issuer": "Issuer",
+                                    "contactPerson": "Contact Person",
+                                    "jobAddress": "Job Address",
+                                    "jobDate": "2023-06-14",
+                                    "startTime": "10:00:00",
+                                    "endTime": "12:00:00",
+                                    "status": null,
+                                    "jobComment": "Job Comment"
+                                }
+                            """))
+                .andExpect(status().isCreated())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Job[] addedJobs = objectMapper.readValue(content, Job[].class);
+        Job addedJob = addedJobs[0];
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/jobs/" + addedJob.getUuid())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+            {
+                                    "jobFormat": "Updated Format",
+                                    "issuer": "Issuer",
+                                    "contactPerson": "Contact Person",
+                                    "jobAddress": "Job Address",
+                                    "jobDate": "2023-06-14",
+                                    "startTime": "10:00:00",
+                                    "endTime": "12:00:00",
+                                    "status": null,
+                                    "jobComment": "Job Comment"
+                                }
+                            """))
+                .andExpect(status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs/" + addedJob.getUuid()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+ {
+                                    "jobFormat": "Updated Format",
+                                    "issuer": "Issuer",
+                                    "contactPerson": "Contact Person",
+                                    "jobAddress": "Job Address",
+                                    "jobDate": "2023-06-14",
+                                    "startTime": "10:00:00",
+                                    "endTime": "12:00:00",
+                                    "status": null,
+                                    "jobComment": "Job Comment"
+                                }
+                    """));
+    }
+
 
 
 }
