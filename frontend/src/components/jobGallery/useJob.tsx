471b90d4a8a37ada796e453c6407ef4ef6514e59
KitNaIn <k.na-in@gmx.de>
2023-06-20 13:51:06 +0200

    adjusted useJobs hook for less confusion useJobs hook returns [jobs] and a tulpe

--- frontend/src/components/jobGallery/useJob.tsx
+++ frontend/src/components/jobGallery/useJob.tsx
@@ -5,17 +5,16 @@ import { Jobs } from './model/Jobs';
 const useJobs = (): [Jobs[], () => void] => {
     const [jobs, setJobs] = useState<Jobs[]>([]);
 
-    const fetchJobs = () => {
+    const fetchJobs = async () => {
         const url = '/api/jobs';
-        axios
-            .get(url)
-            .then((response) => {
-                setJobs(response.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching Jobs', error);
-                setJobs([]);
-            });
+
+        try {
+            const response = await axios.get(url);
+            setJobs(response.data);
+        } catch (error) {
+            console.error('Error fetching Jobs', error);
+            setJobs([]);
+        }
     };
 
     return [jobs, fetchJobs];
