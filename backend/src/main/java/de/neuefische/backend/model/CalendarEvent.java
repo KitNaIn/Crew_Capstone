a5fc1b9576b834ff056a524b48e4a87e578e6264
KitNaIn <k.na-in@gmx.de>
2023-06-21 15:12:52 +0200

    added save method in backend + tests for calendarevent

--- backend/src/main/java/de/neuefische/backend/model/CalendarEvent.java
+++ backend/src/main/java/de/neuefische/backend/model/CalendarEvent.java
@@ -1,6 +1,7 @@
 package de.neuefische.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document("CalendarEvent")
+@Builder
 public class CalendarEvent {
     @Id
     private String uuid;
