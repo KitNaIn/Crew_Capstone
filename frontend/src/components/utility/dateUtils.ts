3469b69e9b5cbb0ba71c47b8eeabcf80cd22e223
KitNaIn <k.na-in@gmx.de>
2023-07-04 16:09:13 +0200

    styling and time/date format for jobgallery

---
+++ frontend/src/components/utility/dateUtils.ts
@@ -0,0 +1,15 @@
+export const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    return `${day}.${month}.${year}`;
+};
+
+export const formatTime = (timeString: string) => {
+    const time = new Date(`2000-01-01T${timeString}`);
+    const hours = time.getHours().toString().padStart(2, '0');
+    const minutes = time.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+};
+
