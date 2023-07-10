2e2b1e5e55a75320cefd7e0ca36f590f3e4d4ce3
KitNaIn <k.na-in@gmx.de>
2023-07-10 16:58:44 +0200

    ## Overall fixes - adjusted css for background - fixed registration form

--- frontend/src/components/security/Home.tsx
+++ frontend/src/components/security/Home.tsx
@@ -1,47 +1,57 @@
-import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
-import {toast} from "react-toastify";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import './Home.css';
 import avatar from './images/avatar.png';
 
 type Props = {
-    login: (username: string, password: string) => Promise<void>,
-    register: (username: string, password: string) => Promise<void>
-}
+    login: (username: string, password: string) => Promise<void>;
+    register: (username: string, password: string) => Promise<void>;
+};
 
 enum ButtonType {
     Login,
-    Register
+    Register,
 }
-function Home(props:Props) {
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [buttonType, setButtonType] = useState<ButtonType | undefined>(undefined)
+enum BoxState {
+    Login,
+    Register,
+}
+
+function Home(props: Props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [buttonType, setButtonType] = useState<ButtonType | undefined>(undefined);
+    const [boxState, setBoxState] = useState<BoxState>(BoxState.Login);
     const navigate = useNavigate();
     const [animate, setAnimate] = useState(false);
 
     function onSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+        e.preventDefault();
         if (buttonType === ButtonType.Login) {
             props.login(username, password)
                 .then(() => {
                     setAnimate(true);
                     setTimeout(() => {
-                        navigate("/dashboard");
+                        navigate('/dashboard');
                     }, 300);
-                }).catch((error) => {
-                toast.error(error.message || "Login failed");
-            });
+                })
+                .catch((error) => {
+                    toast.error(error.message || 'Login failed');
+                });
         } else if (buttonType === ButtonType.Register) {
             props.register(username, password)
                 .then(() => {
-                    navigate("/");
-                }).catch((error) => {
-                toast.error(error.message || "Registration failed");
-            });
+                    navigate('/login');
+                })
+                .catch((error) => {
+                    toast.error(error.message || 'Registration failed');
+
+                });
         }
     }
+
     useEffect(() => {
         if (animate) {
             setTimeout(() => {
@@ -51,11 +61,11 @@ function Home(props:Props) {
     }, [animate]);
 
     function onChangeHandlerUsername(e: ChangeEvent<HTMLInputElement>) {
-        setUsername(e.target.value)
+        setUsername(e.target.value);
     }
 
     function onChangeHandlerPassword(e: ChangeEvent<HTMLInputElement>) {
-        setPassword(e.target.value)
+        setPassword(e.target.value);
     }
 
     function onClickLogin() {
@@ -63,33 +73,113 @@ function Home(props:Props) {
     }
 
     function onClickRegister() {
-        setButtonType(ButtonType.Register);
+        setBoxState(BoxState.Register);
+    }
+
+    function onClickBack() {
+        setBoxState(BoxState.Login);
     }
 
     return (
         <div className={`background ${animate ? 'slide-out' : 'slide-in'}`}>
-        <div className="boxbox">
-            <div className="box">
-                <img src={avatar} alt="Avatar" className="avatar" />
-            <form className="form" onSubmit={onSubmit}>
-                <div className="fields">
-                    <div className="form">
-                        <input className="input" placeholder="Email" required onChange={onChangeHandlerUsername}  type="email"/>
-                            <span className="input-border"></span>
-                        <input className="input" type="password" placeholder="Password" required minLength={3} onChange={onChangeHandlerPassword} />
-                        <span className="input-border"></span>
-                    </div>
-                    <div style={{display:"flex", marginTop:"2vh", alignItems:"center"}}>
-                    <input type="checkbox"/> <p style={{display:"flex",fontSize:"10px", marginRight:"5vw"}}>Remember me</p>
-                    </div>
-                    <button type="submit" className="shadow__btn" onClick={onClickLogin} >Login</button>
+            <div className="boxbox">
+                <div className={`box ${boxState === BoxState.Register ? 'register-box' : ''}`}>
+                    {boxState === BoxState.Login ? (
+                        <>
+                            <img src={avatar} alt="Avatar" className="avatar" />
+                            <form className="form" onSubmit={onSubmit}>
+                                <div className="fields">
+                                    <div className="form">
+                                        <input
+                                            className="input"
+                                            placeholder="Email"
+                                            required
+                                            onChange={onChangeHandlerUsername}
+                                            type="email"
+                                        />
+                                        <span className="input-border"></span>
+                                        <input
+                                            className="input"
+                                            type="password"
+                                            placeholder="Password"
+                                            required
+                                            minLength={3}
+                                            onChange={onChangeHandlerPassword}
+                                        />
+                                        <span className="input-border"></span>
+                                    </div>
+                                    <div style={{ display: 'flex', marginTop: '2vh', alignItems: 'center' }}>
+                                        <input type="checkbox" />{' '}
+                                        <p style={{ display: 'flex', fontSize: '10px', marginRight: '5vw' }}>Remember me</p>
+                                    </div>
+                                    <button type="submit" className="shadow__btn" onClick={onClickLogin}>
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
+                            <p
+                                onClick={onClickRegister}
+                                style={{
+                                    display: 'flex',
+                                    color: 'lightgrey',
+                                    marginTop: '1vh',
+                                    fontSize: '1.5vh',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                Don't have an Account yet? Sign up!
+                            </p>
+                        </>
+                    ) : (
+                        <>
+                            <h2 style={{letterSpacing:'-1px'}}>Register</h2>
+                            <form className="form" onSubmit={onSubmit}>
+                                <div className="fields">
+                                    <div className="form">
+                                        <input
+                                            className="input"
+                                            placeholder="Email"
+                                            required
+                                            onChange={onChangeHandlerUsername}
+                                            type="email"
+                                        />
+                                        <span className="input-border" ></span>
+                                        <input
+                                            className="input"
+                                            type="password"
+                                            placeholder="Password"
+                                            required
+                                            minLength={3}
+                                            onChange={onChangeHandlerPassword}
+                                        />
+                                        <span className="input-border"></span>
+                                    </div>
+                                    <div style={{ display: 'flex', marginTop: '2vh', alignItems: 'center' }}>
+                                        <input type="checkbox" />{' '}
+                                        <p style={{ display: 'flex', fontSize: '10px', marginRight: '5vw' }}>Remember me</p>
+                                    </div>
+                                    <button type="submit" className="shadow__btn" onClick={() => setButtonType(ButtonType.Register) }>
+                                        Register
+                                    </button>
+                                    <p
+                                        onClick={onClickBack}
+                                        style={{
+                                            display: 'flex',
+                                            color: 'lightgrey',
+                                            marginTop: '1vh',
+                                            fontSize: '1.5vh',
+                                            cursor: 'pointer',
+                                            alignSelf:'center'
+                                        }}
+                                    >
+                                        Go back to Login
+                                    </p>
+                                </div>
+                            </form>
+                        </>
+                    )}
                 </div>
-            </form>
             </div>
-            <p onClick={onClickRegister} style={{display: "flex", color: "lightgrey", marginTop: "1vh", fontSize: "1.5vh", cursor: 'pointer'}}>
-                Don't have an Account yet? Sign in!
-            </p>
-        </div>
         </div>
     );
 }
